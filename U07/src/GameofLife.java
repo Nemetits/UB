25510042140b8afc6be86f8e61afd2bdabe77fa2
Michael <michi.rega999@gmail.com>
2021-02-02 16:18:41 +0100

    update

--- U07/src/GameofLife.java
+++ U07/src/GameofLife.java
@@ -1,3 +1,111 @@
-public class GameofLife {
+import java.awt.*;
+import java.applet.*;
 
+public class GameofLife extends Applet {
+    public void paint(Graphics g) {
+        // Fixe Anzahl von Zeilen und Spalten
+        final int ANZAHL_ZEILEN = 11;
+        final int ANZAHL_SPALTEN = 11;
+        // Maximale Anzahl von Iterationsschritten
+        final int MAX_SCHRITTE = 5;
+        boolean[][] matrix1 = new boolean[ANZAHL_ZEILEN][ANZAHL_SPALTEN];
+        matrix1 = fuellenMatrixSternMitte(matrix1, 5);
+        boolean[][] matrix2 = new boolean[ANZAHL_ZEILEN][ANZAHL_SPALTEN];
+        int counter = 0;
+        do {
+            ausgebenMatrix(g, matrix1);
+            matrix2 = matrix1.clone();
+            matrix1 = berechneMatrix(matrix2, ANZAHL_ZEILEN, ANZAHL_SPALTEN);
+            counter++;
+            bremse(1000);
+        } while (counter < MAX_SCHRITTE && exisitierenUnterschiede(matrix1, matrix2));
+    }
+
+    public static boolean[][] fuellenMatrixSternMitte(boolean[][] matrix, int groesse) {
+        boolean[][] ret = matrix;
+        matrix[matrix.length / 2][matrix.length / 2] = true;
+        for (int i = 0; i < groesse; i++) {
+            ret[(matrix.length / 2) + i][matrix[0].length / 2] = true;
+            ret[(matrix.length / 2) - i][matrix[0].length / 2] = true;
+            ret[matrix.length / 2][(matrix[0].length / 2) + i] = true;
+            ret[matrix.length / 2][(matrix[0].length / 2) - i] = true;
+        }
+        return ret;
+    }
+
+    public void ausgebenMatrix(Graphics g, boolean[][] matrix) {
+        int breite = getWidth();
+        int hoehe = getHeight();
+        g.setColor(Color.darkGray);
+        for (int i = 0; i < matrix[0].length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                if (matrix[j][i]) {
+                    g.setColor(Color.red);
+                }
+                g.fillRect((breite + 5) / matrix.length * j, (hoehe + 5) / matrix[0].length * i, (breite - 5) / matrix.length,
+                        (hoehe - 5) / matrix[0].length);
+                g.setColor(Color.darkGray);
+            }
+        }
+    }
+
+    public static int anzahlLebendeNachbaren(boolean[][] matrix1, int zeile, int spalte) {
+        int ret = 0;
+        if (zeile != 0 && spalte != 0 && matrix1[zeile - 1][spalte - 1])
+            ret++;
+        if (zeile != 0 && matrix1[zeile - 1][spalte])
+            ret++;
+        if (zeile != 0 && spalte != matrix1[0].length - 1 && matrix1[zeile - 1][spalte + 1])
+            ret++;
+        if (spalte != 0 && matrix1[zeile][spalte - 1])
+            ret++;
+        if (spalte != matrix1[0].length - 1 && matrix1[zeile][spalte + 1])
+            ret++;
+        if (zeile != matrix1.length - 1 && spalte != 0 && matrix1[zeile + 1][spalte - 1])
+            ret++;
+        if (zeile != matrix1.length - 1 && matrix1[zeile + 1][spalte])
+            ret++;
+        if (zeile != matrix1.length - 1 && spalte != matrix1[0].length - 1 && matrix1[zeile + 1][spalte + 1])
+            ret++;
+        return ret;
+    }
+
+    public static boolean[][] berechneMatrix(boolean[][] matrix, int zeilen, int spalten){
+        boolean[][] ret = new boolean[zeilen][spalten];
+        int nachbaren;
+        for (int i = 0; i < matrix[0].length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                nachbaren = anzahlLebendeNachbaren(matrix, j, i);
+                if (nachbaren < 2 || nachbaren > 3)
+                    ret[j][i] = false;
+                else if (nachbaren == 3)
+                    ret[j][i] = true;
+                else if (matrix[j][i])
+                    ret[j][i] = true;
+            }
+        }
+        return ret;
+    }
+    public static boolean exisitierenUnterschiede (boolean[][] matrix1, boolean[][] matrix2){
+        boolean ret = false;
+        for (int i = 0; i < matrix1[0].length; i++){
+            for (int j = 0; j < matrix2.length; j++){
+                if (matrix1[j][i] != matrix2[j][i]){
+                    ret = true;
+                }
+            }
+        }
+        return ret;
+    }
+
+    /**
+     * Veranlasst dass das Programm millis Millisekunden pausiert
+     * @param millis Anzahl der Millisekunden die gewartet werden
+     */
+    public void bremse(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+        }
+    }
 }
--- U07/src/Kartentrick.java
+++ U07/src/Kartentrick.java
@@ -1,126 +1,155 @@
 
 public class Kartentrick {
 
-	public static void main(String[] args) {
-		System.out.println("Kartentrick");
-		System.out.println("===========");
+    /**
+     * Der Titel wird ausgegeben, das array karten wird instanziert dann wird
+     * fuellen 3 mal, natürlich wird immer ausgegeben und die Spalte gefragt ausgeführt
+     * um die gesuchte karte in der Mitte zu bringen. Die Karte an Stelle 3 1 wird ausgedruckt
+     * und gefragt ob man nochmals will.
+     * * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("Kartentrick");
+        System.out.println("===========");
 
-		do {
-			int[][] karten = { { 1, 8, 15 }, { 2, 9, 16 }, { 3, 10, 17 }, { 4, 11, 18 }, { 5, 12, 19 }, { 6, 13, 20 },
-					{ 7, 14, 21 } };
-			ausgeben(karten);
-			int spalte = TestScannerErweitert.readInt("Spalte der Karte: ");
-			karten = fuellen(karten, spalte);
-			ausgeben(karten);
-			spalte = TestScannerErweitert.readInt("Spalte der Karte: ");
-			karten = fuellen(karten, spalte);
-			ausgeben(karten);
-			spalte = TestScannerErweitert.readInt("Spalte der Karte: ");
-			karten = fuellen(karten, spalte);
-			ausgeben(karten);
-			System.out.println("Karte " + karten[3][1] + " wurde gewählt");
-		} while (Character.toLowerCase(TestScannerErweitert.readChar("Nochmals (j/n)? ")) == 'j');
-	}
+        do {
+            int[][] karten = {{1, 8, 15}, {2, 9, 16}, {3, 10, 17}, {4, 11, 18}, {5, 12, 19}, {6, 13, 20},
+                    {7, 14, 21}};
+            ausgeben(karten);
+            int spalte = TestScannerErweitert.readInt("Spalte der Karte: ");
+            karten = fuellen(karten, spalte);
+            ausgeben(karten);
+            spalte = TestScannerErweitert.readInt("Spalte der Karte: ");
+            karten = fuellen(karten, spalte);
+            ausgeben(karten);
+            spalte = TestScannerErweitert.readInt("Spalte der Karte: ");
+            karten = fuellen(karten, spalte);
+            ausgeben(karten);
+            System.out.println("Karte " + karten[3][1] + " wurde gewählt");
+        } while (Character.toLowerCase(TestScannerErweitert.readChar("Nochmals (j/n)? ")) == 'j');
+    }
 
-	public static int[][] fuellen(int[][] karten, int spalte) {
-		int[][] ret = new int[7][3];
-		int[] reihenfolge = { 0, 1, 2 };
-		int[] arr1 = new int[7];
-		int[] arr2 = new int[7];
-		int[] arr3 = new int[7];
-		if (spalte == 1) {
-			reihenfolge[0] = 2;
-			reihenfolge[1] = 0;
-			reihenfolge[2] = 1;
-		} else if (spalte == 3) {
-			reihenfolge[0] = 1;
-			reihenfolge[1] = 2;
-			reihenfolge[2] = 0;
-		}
-		for (int i = 0; i < 7; i++) {
-			arr1[i] = karten[i][reihenfolge[0]];
-		}
-		for (int i = 0; i < 7; i++) {
-			arr2[i] = karten[i][reihenfolge[1]];
-		}
-		for (int i = 0; i < 7; i++) {
-			arr3[i] = karten[i][reihenfolge[2]];
-		}
-		int counter1 = 0;
-		int counter2 = 0;
-		for (int i = 0; i < 7; i++) {
-			if (i == 3 || i == 6) {
-				counter1 = 0;
-				counter2++;
-			} else if (i == 4) {
-				counter1 = 1;
-			} else if (i == 5) {
-				counter1 = 2;
-			}
-			ret[counter2][counter1] = arr1[i];
-			if (i < 3) {
-				counter1++;
-			}
-		}
+    /**
+     * Bei der fuellen Methode wird der zweidimensionale array in einer anderen Reihenfolge
+     * geschrieben, die spalte gibt an ab wo und wie das array umgestellt werden soll
+     * @param karten ist der übergebene array der umgestellt werden soll
+     * @param spalte ist ab wo er ugestellt werden soll
+     * @return gibt das umgestellte array zurück
+     */
+    public static int[][] fuellen(int[][] karten, int spalte) {
+//        ein array das gleich wie das karten array ist wird instanziert
+        int[][] ret = new int[7][3];
+//        ein array indem die Reihenfolge der umstellung der spalten wird instanziert
+        int[] reihenfolge = {0, 1, 2};
+//        drei arrays für die
+        int[] arr1 = new int[7];
+        int[] arr2 = new int[7];
+        int[] arr3 = new int[7];
+//        die Reihenfoge der Spalten wird in den reihenfolgen array geschrieben
+        if (spalte == 1) {
+            reihenfolge[0] = 2;
+            reihenfolge[1] = 0;
+            reihenfolge[2] = 1;
+        } else if (spalte == 3) {
+            reihenfolge[0] = 1;
+            reihenfolge[1] = 2;
+            reihenfolge[2] = 0;
+//            die spalten werden nach reihenfolge in den drei arrays geschrieben
+        }
+        for (int i = 0; i < 7; i++) {
+            arr1[i] = karten[i][reihenfolge[0]];
+        }
+        for (int i = 0; i < 7; i++) {
+            arr2[i] = karten[i][reihenfolge[1]];
+        }
+        for (int i = 0; i < 7; i++) {
+            arr3[i] = karten[i][reihenfolge[2]];
+        }
+//        das array mit der ersten Spalte wird in der ret array geschrieben. Mit
+//        countern wird nach drei spalten immer eine neue Reihe genutzt
+        int counter1 = 0;
+        int counter2 = 0;
+        for (int i = 0; i < 7; i++) {
+            if (i == 3 || i == 6) {
+                counter1 = 0;
+                counter2++;
+            } else if (i == 4) {
+                counter1 = 1;
+            } else if (i == 5) {
+                counter1 = 2;
+            }
+            ret[counter2][counter1] = arr1[i];
+            if (i < 3) {
+                counter1++;
+            }
+        }
+//        die counter werden zur stelle erhöht bei der das erste Element des zweiten
+//        arrays sein soll
+        counter1 = 1;
+        counter2 = 2;
+        for (int i = 1, j = 0; i < 9 && j < 7; i++, j++) {
+            if (i == 3) {
+                counter1 = 0;
+                counter2++;
+            } else if (i == 6) {
+                counter1 = 1;
+                counter2++;
+            } else if (i == 5) {
+                counter1 = 2;
+            } else if (i == 4) {
+                counter1 = 1;
+            }
+            ret[counter2][counter1] = arr2[j];
+            if (i < 3) {
+                counter1++;
+            }
+//            das ELement an Spalte 0 Reihe 4 wird hinzugefügt
+            ret[4][0] = arr2[5];
 
-		counter1 = 1;
-		counter2 = 2;
-		for (int i = 1, j = 0; i < 9 && j < 7; i++, j++) {
-			if (i == 3) {
-				counter1 = 0;
-				counter2++;
-			} else if (i == 6) {
-				counter1 = 1;
-				counter2++;
-			} else if (i == 5) {
-				counter1 = 2;
-			} else if (i == 4) {
-				counter1 = 1;
-			}
-			ret[counter2][counter1] = arr2[j];
-			if (i < 3) {
-				counter1++;
-			}
-			ret[4][0] = arr2[5];
+        }
+//        die counter werden zur stelle erhöht bei der das erste Element des dritten
+//        arrays sein soll
+        counter1 = 2;
+        counter2 = 4;
+        for (int i = 2, j = 0; i < 7 && j < 7; i++, j++) {
+            if (i == 3 || i == 6) {
+                counter1 = 0;
+                counter2++;
+            } else if (i == 4) {
+                counter1 = 1;
+            } else if (i == 5) {
+                counter1 = 2;
+            }
+            ret[counter2][counter1] = arr3[j];
+            if (i < 3) {
+                counter1++;
+            }
+        }
+//        die Elemente an Stelle 6 1 und 6 2 werden hinzugefügt
+        ret[6][1] = arr3[5];
+        ret[6][2] = arr3[6];
+        return ret;
+    }
 
-		}
-		counter1 = 2;
-		counter2 = 4;
-		for (int i = 2, j = 0; i < 7 && j < 7; i++, j++) {
-			if (i == 3 || i == 6) {
-				counter1 = 0;
-				counter2++;
-			} else if (i == 4) {
-				counter1 = 1;
-			} else if (i == 5) {
-				counter1 = 2;
-			}
-			ret[counter2][counter1] = arr3[j];
-			if (i < 3) {
-				counter1++;
-			}
-		}
-		ret[6][1] = arr3[5];
-		ret[6][2] = arr3[6];
-		return ret;
-	}
-/**
- * 
- * @param karten ist das array das ausgedruckt wird wird
- */
-	public static void ausgeben(int[][] karten) {
-		for (int i = 0; i < 7; i++) {
-			for (int j = 0; j < 3; j++) {
-				if (karten[i][j] < 10) {
-					System.out.print("  " + karten[i][j]);
-				} else {
-					System.out.print(" " + karten[i][j]);
-				}
+    /**
+     * Das array wird mit zwei for loops ausgegeben. Wenn die Zahl kleiner Zehn ist
+     * dann werden zwei Leerzeichen vorher ausgedrückt wenn die Zahl gräßer Zehn ist
+     * dann wird ein Leerzeichen davor ausgegeben.
+     * @param karten ist das array das ausgedruckt wird wird
+     */
+    public static void ausgeben(int[][] karten) {
+        for (int i = 0; i < 7; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (karten[i][j] < 10) {
+                    System.out.print("  " + karten[i][j]);
+                } else {
+                    System.out.print(" " + karten[i][j]);
+                }
 
-			}
-			System.out.println();
-		}
+            }
+            System.out.println();
+        }
 
-	}
+    }
 
 }
